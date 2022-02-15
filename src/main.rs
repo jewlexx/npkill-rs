bdeacfad61d740b099a6a9a913d7fb96f03b0193
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-02-15 11:07:12 +1100

    Fix

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::{env, fs, path::PathBuf};
 
 fn main() {
     let current_dir = env::current_dir().unwrap();
+    let mut args = env::args();
+    let dir_name = args.nth(1).unwrap_or("node_modules".to_string());
 
-    let mut files: Vec<PathBuf> = vec![];
+    let mut files: Vec<PathBuf> = Vec::new();
 
     println!("Reading files in {}", &current_dir.display());
 
@@ -11,7 +13,7 @@ fn main() {
         let entry = entry.unwrap();
         let path = entry.path();
 
-        if path.ends_with("node_modules") {
+        if path.ends_with(&dir_name) {
             files.push(path);
         } else {
             let meta = fs::metadata(&path).unwrap();
