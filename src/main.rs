e78da7e10a0edde76498b73aeb09176667205667
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-02-17 13:11:16 +1100

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,14 @@
 use spinners_rs::{Spinner, Spinners};
-use std::{collections::HashMap, env, path::PathBuf, str::FromStr};
+use std::{env, path::PathBuf, str::FromStr};
 use threadpool::Builder as threadpool_Builder;
 
 mod utils;
 use utils::*;
 
 fn main() {
-    let mut args = HashMap::<String, String>::new();
+    let args = args::parse_args();
 
-    for (i, el) in env::args().enumerate() {
-        if i == 0 {
-            continue;
-        }
-
-        if el.starts_with("--") {
-            if el.contains('=') {
-                let mut el = el.splitn(2, '=');
-                let key = el.next().unwrap();
-                let value = el.next().unwrap();
-
-                args.insert(key.to_string(), value.to_string());
-                continue;
-            }
-            args.insert(
-                el.to_string(),
-                env::args().enumerate().nth(i + 1).unwrap().1,
-            );
-        }
-    }
-
-    let current_dir = PathBuf::from_str(
-        args.get("--dir")
-            .unwrap_or(&env::current_dir().unwrap().display().to_string()),
-    )
-    .unwrap();
+    let current_dir = PathBuf::from_str(&args.dir).unwrap();
 
     let mut dirs: Vec<PathBuf> = Vec::new();
 
